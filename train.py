6abac8bb41f69699551dc5dcc0390e913bbd0d14
Sanan Mehdiyev <senanmehdiyev25@gmail.com>
2024-12-22 03:19:23 +0400

    new upds

--- train.py
+++ train.py
@@ -12,12 +12,12 @@ def func_train():
     config = load_config()
 
     # Read the dataset
-    try:
-        data = retrieve_data_from_azure()
-        print('raw data successfully loaded', len(data))
+    # try:
+    data = retrieve_data_from_azure()
+    print('raw data successfully loaded', len(data))
 
-    except Exception as e:
-        print(f"raw data CANNOT loaded due to {e}")
+    # except Exception as e:
+    #     print(f"raw data CANNOT loaded due to {e}")
 
     # Define input and target columns
     X = data[[col for col in config['preprocessor']['categorical']['features']] + [col for col in config['preprocessor']['numerical']['features']]]
