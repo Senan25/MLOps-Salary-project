54cc0c785e8427c0ae0922ac5752ec82ba8fea8d
Senan Stefan Mehdiyev <77478926+Senan25@users.noreply.github.com>
2025-02-01 20:30:20 +0400

    Update model_logging.py

--- model_logging.py
+++ model_logging.py
@@ -4,13 +4,20 @@ from datetime import datetime
 import json
 import pytz
 
+# Get MLflow Tracking Server IP from environment variable
 ip = os.getenv("mlflow_track_ip")
-ip = "https://" + ip
-print(ip)
+if ip:
+    ip = "https://" + ip
+else:
+    raise ValueError("Environment variable 'mlflow_track_ip' is not set")
+
+print(f"MLflow Tracking URI: {ip}")
 
 # MLflow configuration
-mlflow.set_tracking_uri(ip)  # Replace with your remote tracking server URI
+mlflow.set_tracking_uri(ip)  # Set the remote tracking server URI
+
 EXPERIMENT_NAME = "AUTO_PIPELINE"
+MODEL_NAME = EXPERIMENT_NAME + "_MODEL"  # Dynamic model name based on the experiment
 mlflow.set_experiment(EXPERIMENT_NAME)
 
 # Define paths
@@ -18,25 +25,36 @@ model_path = 'serve/rf_model.pkl'  # Path to your pre-trained model
 best_params = 'best_params.json'
 metrics_path = 'evaluation_results.json'
 
-if not os.path.exists(model_path):
-    raise FileNotFoundError(f"Model file not found at {model_path}")
-if not os.path.exists(best_params):
-    raise FileNotFoundError(f"Best parameters file not found at {best_params}")
+# Validate required files exist
+for file in [model_path, best_params, metrics_path]:
+    if not os.path.exists(file):
+        raise FileNotFoundError(f"Required file not found: {file}")
 
+# Get current time in Baku timezone
 baku_tz = pytz.timezone('Asia/Baku')
 current_time = datetime.now(baku_tz).strftime("%Y-%m-%d_%H:%M:%S")
 run_name = f"{current_time}_run"
 
 with mlflow.start_run(run_name=run_name) as run:
-    # Log the model
+    # Log artifacts (model, best params, and dvc.lock)
     mlflow.log_artifact(model_path, artifact_path="models")
     mlflow.log_artifact(best_params, artifact_path="models")
     mlflow.log_artifact("dvc.lock", artifact_path="models")
 
+    # Log evaluation metrics
     with open(metrics_path, 'r') as f:
         params = json.load(f)
         for key, value in params.items():
             if isinstance(value, (int, float)):  # Log only numeric values as metrics
-                mlflow.log_metric(key, value)   
+                mlflow.log_metric(key, value)
 
-    print(f"Run ID: {run.info.run_id}")
+    # Get Run ID
+    run_id = run.info.run_id
+    print(f"Run ID: {run_id}")
+
+    # Model registry URI (Artifacts path for this run)
+    model_uri = f"runs:/{run_id}/models"
+
+    # Register the model
+    registered_model = mlflow.register_model(model_uri, MODEL_NAME)
+    print(f"Model registered with name: {registered_model.name} and version: {registered_model.version}")
